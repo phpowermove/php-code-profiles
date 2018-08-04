21591d402eb1fabe8e767cded28bf01c34a24581
gossi <gossi@users.noreply.github.com>
2018-08-04 17:40:36 +0200

    imported from gossi/php-code-formatter

---
+++ src/YamlLoader.php
@@ -0,0 +1,17 @@
+<?php
+namespace gossi\code\profiles;
+
+use Symfony\Component\Config\Loader\FileLoader;
+use Symfony\Component\Yaml\Yaml;
+
+class YamlLoader extends FileLoader {
+
+	public function load($resource, $type = null) {
+		return Yaml::parse(file_get_contents($resource));
+	}
+
+	public function supports($resource, $type = null) {
+		return is_string($resource)
+			&& in_array(pathinfo($resource, PATHINFO_EXTENSION), ['yml', 'yaml']);
+	}
+}
